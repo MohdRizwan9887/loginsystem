4bbf3f9d5b83f9f5f0ade9536bda989f72806cd1
MohdRizwan9887 <102258150+MohdRizwan9887@users.noreply.github.com>
2025-01-20 20:15:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <a href="https://www.darik_blog1.com/">BLOG Website</a>
     <title>Blogging-for blog</title>
     <link rel="stylesheet" href="peace.css">
 </head>
